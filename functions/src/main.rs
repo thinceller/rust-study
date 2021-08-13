fe6017877d40b8701206e0cb3faa803b85c7888d
thinceller <thinceller@gmail.com>
2021-08-13 20:42:48 +0900

    add functions section

---
+++ functions/src/main.rs
@@ -0,0 +1,13 @@
+fn main() {
+    let x = plus_one(5);
+    another_function(x, 6);
+}
+
+fn another_function(x: i32, y: i32) {
+    println!("The value of x is: {}", x);
+    println!("The value of y is: {}", y);
+}
+
+fn plus_one(x: i32) -> i32 {
+    x + 1
+}
